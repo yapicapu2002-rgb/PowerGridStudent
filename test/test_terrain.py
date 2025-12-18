f371e7101ba7f9a28e6c8612bf7b0b7f50cb737b
kerone <yapicapu2002@gmail.com>
2025-12-18 12:28:40 +0100

    Modification du test terrain

--- test/test_terrain.py
+++ test/test_terrain.py
@@ -7,8 +7,20 @@ from Terrain import Terrain, Case
 class TestTerrain(unittest.TestCase):
 
     def test_chargement(self):
-        # TODO
-        self.fail()
+          # Création d’un fichier terrain temporaire
+        contenu = ["E..\n","CCC\n"]
+
+        with open("terrain_test.txt", "w") as f:
+            f.writelines(contenu)
+
+        t = Terrain()
+        t.charger("terrain_test.txt")
+
+        self.assertEqual(t.cases[0][0], Case.ENTREE)
+        self.assertEqual(t.cases[0][1], Case.VIDE)
+        self.assertEqual(t.cases[1][0], Case.CLIENT)
+        self.assertEqual(t.cases[1][2], Case.CLIENT)
+        
 
     def test_accesseur(self):
         t = Terrain()
